ef2604cfce112799eae98e7e82c68781ea4a3a7c
George <walhallyus@gmail.com>
2026-03-24 11:50:28 +0200

    fix sitemap and robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: "/",
       },
     ],
-    sitemap: "https://overseek.net/sitemap.xml",
+    sitemap: "https://www.overseek.net/sitemap.xml",
   };
 }
